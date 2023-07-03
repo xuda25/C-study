05f76ac4154f0d92ec4ec309fe3f71557d9b29f8
xuda25 <1322680618@qq.com>
2023-07-03 16:50:28 +0800

    2023/7/3

--- data structure/Link_List.cpp
+++ data structure/Link_List.cpp
@@ -28,7 +28,11 @@ public:
             p = p->next;
         }
     }
-
+    // 测试用 获得节点
+    Node* getHead()
+    {
+        return head;
+    }
 
 
     // 按位序插入（带头节点）
@@ -60,7 +64,7 @@ public:
     // 实现后插操作  在节点p后 插入e
     bool InsertNextNode(Node* p, int e)
     {
-        if (p == nullptr) return false;
+        if (p == nullptr || head->next == nullptr) return false;
         
         Node* h = head->next;
         while (h != p)
@@ -73,9 +77,80 @@ public:
         Node* nw = new Node(e);
         nw->next = h->next;
         h->next = nw;
-        +
+        ++length;
+        return true;
     }
+    //实现前插操作
+    bool InsertPriorNode(Node* p, int e)
+    {
+        if (p == nullptr || head->next == nullptr) return false;
+        // 寻找p前一个节点
+        Node* h = head;
 
+        while (h->next != p)
+        {
+            h = h->next;
+            if (h == nullptr)
+                return false;
+        }
+        //如果找到 完成前插
+        Node* nw = new Node(e);
+        nw->next = h->next;
+        h->next = nw;
+        ++length;
+        return true;
+    }
+    // 按位序删除
+    bool ListDelete(int i, int& r)  // 删除i位置数，  将i位置数通过r返回
+    {   
+        if (i < 1 || i > length) return false;
+        // 1.找到i-1位置节点
+        Node* h = head;
+        int count = 0; // 计数
+        while (count < i-1)
+        {
+            h = h->next;
+            ++count;
+        }
+        //2.删除i 返回数值
+        r = h->next->data;
+        Node* de = h->next;
+        h->next = h->next->next;
+        delete(de);
+        --length;
+        return true;
+    }
+    // 删除指定节点
+    bool DeleteNode(Node* p)
+    {
+        Node* d = p->next;
+        p->data = d->data;
+        p->next = d->next;
+        delete(d);
+        --length;
+        return true;
+    }
+    // 按位查找， 返回节点
+    Node* GetElem(int i)
+    {
+        if (i < 1 || i > length) return nullptr;
+        Node* h = head;
+        int count = 0;
+        while (count < i)
+        {
+            h = h->next;
+            ++count;
+        }
+        return h;
+    }
+    // 按值查找 返回节点
+    Node* LocateElem(int e)
+    {
+        Node* h = head;
+        while (h != nullptr && h->data != e)
+            h = h->next;
+        return h;
+    }
 private:
     Node* head;
     int length;
@@ -84,9 +159,27 @@ private:
 int main()
 {
     LinkList a;
-    a.ListInsert(1,10);
-    a.ListInsert(2,10);
-    a.ListInsert(5,10);
+    Node* head = a.getHead();
+    a.ListInsert(1,1);
+    a.ListInsert(2,2);
+    a.ListInsert(3,3);
+    Node* one = head->next;
+    //a.test();
+    a.InsertNextNode(one, 20);
     a.test();
-
+    cout << endl;
+    a.InsertPriorNode(one, 111);
+    a.test();
+    cout << endl;
+    int b;
+    a.ListDelete(5, b);
+    a.test();
+    cout << endl;
+    a.DeleteNode(one);
+    a.test();
+    cout << endl;
+    Node*c = a.GetElem(1);
+    cout << c->data << endl;
+    Node*d = a.LocateElem(111);
+    cout << d->data << endl;
 }
