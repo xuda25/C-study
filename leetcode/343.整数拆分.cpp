60ef52246ab97cf70ff40a96a74d63167696a607
xuda25 <1322680618@qq.com>
2023-07-05 15:30:58 +0800

    2023/7/5

---
+++ leetcode/343.整数拆分.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=343 lang=cpp
+ *
+ * [343] 整数拆分
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n+1);
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; ++i)
+            for (int j = 1; j < i - 1; ++j)
+                dp[i] = max(max(j*(i-j), j*dp[i-j]), dp[i]);
+
+        return dp[n];
+    }
+};
+// @lc code=end
+
