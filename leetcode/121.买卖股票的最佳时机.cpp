ef0c9eb847346d083df597af1480dfdf6f77c93d
xuda25 <1322680618@qq.com>
2023-07-25 16:24:40 +0800

    2023/7/25

--- leetcode/121.买卖股票的最佳时机.cpp
+++ leetcode/121.买卖股票的最佳时机.cpp
@@ -3,54 +3,48 @@
  *
  * [121] 买卖股票的最佳时机
  */
-
+// 贪心
 // @lc code=start
-
-// 暴力 超时
 // class Solution {
 // public:
 //     int maxProfit(vector<int>& prices) {
-//         int ans = INT32_MIN;
+//         int low = 10001;
+//         int result = 0;
 
 //         for (int i = 0; i < prices.size(); ++i)
 //         {
-//             for (int j = i + 1; j < prices.size(); ++j)
-//             {
-//                 if (prices[j] - prices[i] > ans)
-//                     ans = prices[j] - prices[i];
-//             }
+//             low = min(low, prices[i]);
+//             result = max(result, prices[i] - low);
 //         }
 
-//         if (ans < 0)
-//             return 0;
-        
-//         return ans;
+//         return result;
 //     }
 // };
 
+// 动态规划
 
+// 1. 第i天 dp[i][0] 表示持有股票时拥有的最大金额
+//    dp[i][1] 表示第i天 不持有股票时拥有的最大金额
+// 2. dp[i][0] 可能第i天买入  也可能第i-1天就拥有 dp[i][0] = max(-prices[i], dp[i-1][0])
+    //dp[i][1] 第i天卖出  也可能早就卖出了  就不持有了  dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
+// 3. dp[0][0] = -prices[0];
+      //dp[0][1] = 0;
+// 4. 前到后    
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans = INT32_MIN;
-        int max = prices.size() - 1;
+        vector<vector<int>> dp(prices.size(), vector<int>(2, 0));
+
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
 
         for (int i = 1; i < prices.size(); ++i)
         {
-            if (prices[i] > prices[max])
-                max = i;
+            dp[i][0] = max(-prices[i], dp[i-1][0]);
+            dp[i][1] = max(dp[i-1][1], dp[i][0] + prices[i]); 
         }
 
-        for (int j = 0; j < max; ++j)
-        {
-            if (prices[max] - prices[j] > ans)
-                ans = prices[max] - prices[j];
-        }
-
-        if (ans < 0)
-            return 0;
-        
-        return ans;
+        return dp[prices.size()-1][1];
     }
 };
 // @lc code=end
