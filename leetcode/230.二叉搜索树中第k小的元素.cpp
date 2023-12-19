8679b01387c7718404cc554db5a331d5be30cc5c
xuda25 <1322680618@qq.com>
2023-12-19 15:27:33 +0800

    2023/12/19

--- leetcode/230.二叉搜索树中第k小的元素.cpp
+++ leetcode/230.二叉搜索树中第k小的元素.cpp
@@ -18,8 +18,25 @@
  */
 class Solution {
 public:
+// 中序遍历 存入数组 然后找第k-1处
     int kthSmallest(TreeNode* root, int k) {
-
+        stack<TreeNode*> st;
+        
+        while (root || !st.empty())
+        {
+            while (root)
+            {
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            --k;
+            if (k == 0)
+                break;
+            root = root->right;
+        }
+        return root->val;
     }
 };
 // @lc code=end
